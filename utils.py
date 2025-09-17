a0ccebe06b73de7b33f69debbdb7ee552f47bd57
EGdlP <egom802@aucklanduni.ac.nz>
2025-09-17 14:38:26 +1200

    Changed to 2 decimals

--- utils.py
+++ utils.py
@@ -133,16 +133,17 @@ def calc_retreat(all_merged, c_adjust = 0.5):
         all_merged[slr_quantiles]
         .div(denom , axis=0)
         .rename(columns=lambda c: f"retreat_{c}")
+        .round(2)  # ensures 3 decimals
     )
 
     # Append retreat columns
-    merged_retreat_df = pd.concat([all_merged, retreat_df], axis=1)
+    # merged_retreat_df = pd.concat([all_merged, retreat_df], axis=1)
 
      #(Optional) Historic rate adjustment. Trend is in (m/year)
-    # historic_retreat_df = retreat_df.add(
-    #     (all_merged["year"] - 2005) * all_merged["trend"], axis=0
-    # )
-    # merged_retreat_df = pd.concat([all_merged, historic_retreat_df], axis=1)
+    historic_retreat_df = retreat_df.add(
+        (all_merged["year"] - 2005) * all_merged["trend"].round(2), axis=0
+    )
+    merged_retreat_df = pd.concat([all_merged, historic_retreat_df], axis=1)
 
     return merged_retreat_df
 
@@ -259,9 +260,9 @@ for year in years:
 
         #% From points to linestrings, careful on what active geometry goes inside
         lines_gdf = lines_to_points(subset.set_geometry('geom_new_points'))
-        lines_gdf.to_file(f"lines_shoreline_{slr_qt}qtl_{year}_{scenario}.geojson")
+        lines_gdf.to_file(f"htrend_lines_shoreline_{slr_qt}qtl_{year}_{scenario}.geojson")
         cols_to_display= ['geom_new_points','50','retreat_50']
-        subset[cols_to_display].to_file(f"points_shoreline_{slr_qt}qtl_{year}_{scenario}.geojson")
+        subset[cols_to_display].to_file(f"htrend_points_shoreline_{slr_qt}qtl_{year}_{scenario}.geojson")
 
 #%%
 # Plot new points, compare to ref
