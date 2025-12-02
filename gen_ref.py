020fa7e7d64478d1f2bab15474ab3557798a274c
EGdlP <egom802@aucklanduni.ac.nz>
2025-12-03 09:07:32 +1300

    Revert "Beach slope update: Nick changes"
    This reverts commit d483bc201a61a8c1ed5c1dee94989dce2e0c94bb.

--- gen_ref.py
+++ gen_ref.py
@@ -203,9 +203,8 @@ rivers= gpd.read_file("./preprocessing/nz-river-polygons-topo-150k.gpkg")
 rivers= gpd.GeoDataFrame(rivers, geometry="geometry")
 
 #Intersect with rivers
-# shoreline_ref_noriv, buffered = remove_river_influence(shoreline_ref,rivers,
-#                                                         influence_area = 1000)
-shoreline_ref_noriv = shoreline_ref
+shoreline_ref_noriv, buffered = remove_river_influence(shoreline_ref,rivers,
+                                                        influence_area = 1000)
 
 #Export points
 shoreline_ref_noriv.to_crs(CRS_WGS84).to_file(f'points_ref_shoreline_{custom_ref_year}.geojson')
